833b4a3c21d77a5be33aedf43042d3aeb50f61e0
Callum Bugajski <callumbugajski@hotmail.com>
2019-07-25 12:21:47 +0100

    Added first part of car tests
    Part-of: <https://gitlab.com/beautifulcanoe/developers/traffic3d/merge_requests/22>

---
+++ Traffic3D/Assets/Tests/CarTests.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using System;
+
+public class CarTests {
+
+    [SetUp]
+    public void SetUpTest()
+    {
+        try
+        {
+            SceneManager.LoadScene(0);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+	[UnityTest]
+	public IEnumerator CarCounterTest() {
+
+        int currentCarCount = CarCounter.getCarCount();
+
+        currentCarCount++;
+        CarCounter.incrementCarCount();
+
+        Assert.AreEqual(CarCounter.getCarCount(), currentCarCount);
+
+        currentCarCount--;
+        CarCounter.decrementCarCount();
+
+        Assert.AreEqual(CarCounter.getCarCount(), currentCarCount);
+
+        currentCarCount++;
+        CarCounter.incrementCarCount();
+
+        currentCarCount = 0;
+        CarCounter.resetCarCount();
+
+        Assert.AreEqual(CarCounter.getCarCount(), currentCarCount);
+
+		yield return null;
+
+	}
+
+}
