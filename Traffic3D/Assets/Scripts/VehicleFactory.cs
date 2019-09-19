51fdf2a4e44253b96505096444657ead6d8cb1aa
Callum Bugajski <callumbugajski@hotmail.com>
2019-09-19 15:45:34 +0100

    Temporarily hide vehicles
    Part-of: <https://gitlab.com/beautifulcanoe/developers/traffic3d/merge_requests/49>

--- Traffic3D/Assets/Scripts/VehicleFactory.cs
+++ Traffic3D/Assets/Scripts/VehicleFactory.cs
@@ -10,6 +10,7 @@ public class VehicleFactory : MonoBehaviour
     public float lowRangeRespawnTime = 3;
     public float maximumVehicleCount = 8;
     public float slowDownVehicleRateAt = 6;
+    public float timeOfStartInvisibility = 1;
     public List<Rigidbody> vehicles;
     public List<Path> paths;
     public Dictionary<Rigidbody, Path> currentVehicles = new Dictionary<Rigidbody, Path>();
@@ -25,10 +26,10 @@ public class VehicleFactory : MonoBehaviour
         {
             throw new System.Exception("No paths for vehicles to spawn on.");
         }
-        StartCoroutine(GenerateCars());
+        StartCoroutine(GenerateVehicle());
     }
 
-    IEnumerator GenerateCars()
+    IEnumerator GenerateVehicle()
     {
         while (true)
         {
@@ -56,6 +57,7 @@ public class VehicleFactory : MonoBehaviour
     public Rigidbody SpawnVehicle(Rigidbody vehicle, Path path)
     {
         Rigidbody spawnedVehicle = Instantiate(vehicle, path.nodes[0].position, path.nodes[0].rotation);
+        TemporarilyHideVehicle(spawnedVehicle);
         VehicleEngine vehicleEngine = spawnedVehicle.GetComponent<VehicleEngine>();
         vehicleEngine.SetPath(path);
         currentVehicles.Add(spawnedVehicle, path);
@@ -80,5 +82,17 @@ public class VehicleFactory : MonoBehaviour
             return null;
         }
         return unusedPaths[Random.Range(0, unusedPaths.Count - 1)];
+    }
+
+    public void TemporarilyHideVehicle(Rigidbody vehicle)
+    {
+        vehicle.GetComponentsInChildren<Renderer>().ToList().ForEach(renderer => renderer.enabled = false);
+        StartCoroutine(ShowVehicle(vehicle));
+    }
+
+    IEnumerator ShowVehicle(Rigidbody vehicle)
+    {
+        yield return new WaitForSeconds(timeOfStartInvisibility);
+        vehicle.GetComponentsInChildren<Renderer>().ToList().ForEach(renderer => renderer.enabled = true);
     }
 }
