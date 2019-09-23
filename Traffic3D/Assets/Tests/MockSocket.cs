674a2001233011fded94e11988b6b06c0794faf9
Callum Bugajski <callumbugajski@hotmail.com>
2019-09-23 16:46:30 +0100

    Changed MockSocket to send screenshot path at beginning of connection
    Part-of: <https://gitlab.com/traffic3d/traffic3d/merge_requests/59>

--- Traffic3D/Assets/Tests/MockSocket.cs
+++ Traffic3D/Assets/Tests/MockSocket.cs
@@ -1,7 +1,17 @@
 ﻿using System.Text;
+using UnityEngine;
 
 class MockSocket : ISocket
 {
+    private int receiveCounter;
+    private int sendCounter;
+
+    public MockSocket()
+    {
+        receiveCounter = 0;
+        sendCounter = 0;
+    }
+
     public void Connect(string host, int port)
     {
         // Nothing to connect to
@@ -9,13 +19,22 @@ class MockSocket : ISocket
 
     public int Receive(byte[] buffer)
     {
-        buffer = Encoding.UTF8.GetBytes("0");
+        if (receiveCounter == 0)
+        {
+            buffer = Encoding.UTF8.GetBytes(System.IO.Path.Combine(Application.dataPath, "Screenshots"));
+        }
+        else
+        {
+            buffer = Encoding.UTF8.GetBytes("0");
+        }
+        receiveCounter++;
         return buffer.Length;
     }
 
     public int Send(byte[] buffer)
     {
-        buffer = Encoding.UTF8.GetBytes("Screenshot/shot1.png");
+        sendCounter++;
+        buffer = Encoding.UTF8.GetBytes("shot" + sendCounter + ".png");
         return buffer.Length;
     }
 }
