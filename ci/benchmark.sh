47a09400c50d7ff59e71b51d1b1ccc8ca293dbf2
Callum Bugajski <callumbugajski@hotmail.com>
2019-12-05 11:22:24 +0000

    Trying without multitime
    Part-of: <https://gitlab.com/traffic3d/traffic3d/merge_requests/70>

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
+set -e
+set -x
 echo "Running Benchmark"
-/usr/local/bin/multitime -n 30 "${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
   -projectPath $(pwd)/Traffic3D \
   -executeMethod Benchmark.RunBenchmark \
   -CacheServerIPAddress 172.17.0.1:8126 \
   -logFile \
-  -batchmode | ts '[%Y-%m-%d %H:%M:%S]'"
+  -batchmode | ts '[%Y-%m-%d %H:%M:%S]'
