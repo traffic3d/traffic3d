bdf40fda1165364fd6086db54d83ae6f70255a48
Callum Bugajski <callumbugajski@hotmail.com>
2019-12-05 10:15:18 +0000

    Removed infinite loop and upped benchmark to 30
    Part-of: <https://gitlab.com/traffic3d/traffic3d/merge_requests/70>

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 echo "Running Benchmark"
-chmod +x ./ci/benchmark.sh
-/usr/local/bin/multitime -b ./ci/benchmark.sh -n 5
+chmod +x ./ci/benchmark_run.sh
+/usr/local/bin/multitime -b ./ci/benchmark_run.sh -n 30
