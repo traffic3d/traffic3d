56bff750280735e7063c2dff448e3e6ac29b8a84
Callum Bugajski <callumbugajski@hotmail.com>
2019-12-05 09:51:23 +0000

    Moved into bash file, also found there was an extra \ in unity command
    Part-of: <https://gitlab.com/traffic3d/traffic3d/merge_requests/70>

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
-
-set -e
-set -x
-
 echo "Running Benchmark"
-
-/usr/local/bin/multitime -n 5 "${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
-  -projectPath $(pwd)/Traffic3D \
-  -executeMethod Benchmark.RunBenchmark \ \
-  -CacheServerIPAddress 172.17.0.1:8126 \
-  -logFile \
-  -batchmode | ts '[%Y-%m-%d %H:%M:%S]'"
+chmod +x ./ci/benchmark.sh
+/usr/local/bin/multitime -b ./ci/benchmark.sh -n 5
---
+++ ci/benchmark_run.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+echo "Running batch file"
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+  -projectPath $(pwd)/Traffic3D \
+  -executeMethod Benchmark.RunBenchmark \
+  -CacheServerIPAddress 172.17.0.1:8126 \
+  -logFile \
+  -batchmode | ts '[%Y-%m-%d %H:%M:%S]'
