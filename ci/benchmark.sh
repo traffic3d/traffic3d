bc785abea959a35492d48e31cba45fd3ecd7b000
Callum Bugajski <callumbugajski@hotmail.com>
2019-12-12 15:47:11 +0000

    Added Multitime
    Part-of: <https://gitlab.com/traffic3d/traffic3d/merge_requests/70>

--- ci/benchmark.sh
+++ ci/benchmark.sh
@@ -1,12 +1,5 @@
-#!/usr/bin/env bash
+#!/usr/bin/sh
 set -e
 set -x
 echo "Running Benchmark"
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
-  -projectPath $(pwd)/Traffic3D \
-  -runTests \
-  -testPlatform playmode \
-  -testFilter RunBenchmarkTest \
-  -CacheServerIPAddress 172.17.0.1:8126 \
-  -logFile \
-  -batchmode | ts '[%Y-%m-%d %H:%M:%S]'
+/usr/local/bin/multitime -n 5 sh benchmarkrun.sh
