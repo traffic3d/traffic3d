d213a1d0bea16d40e195a300de9f054a3bac9990
Callum Bugajski <callumbugajski@hotmail.com>
2019-12-13 09:38:10 +0000

    Added headless mode into CI script
    Part-of: <https://gitlab.com/traffic3d/traffic3d/merge_requests/71>

--- ci/benchmarkrun.sh
+++ ci/benchmarkrun.sh
@@ -2,4 +2,4 @@
 set -e
 set -x
 echo "Running Unity"
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity}  -projectPath $(pwd)/Traffic3D  -runTests  -testPlatform playmode  -testFilter RunBenchmarkTest  -CacheServerIPAddress 172.17.0.1:8126  -logFile  -batchmode
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity}  -projectPath $(pwd)/Traffic3D  -runTests  -testPlatform playmode  -testFilter RunBenchmarkTest  -executeMethod HeadlessMode.Run  -CacheServerIPAddress 172.17.0.1:8126  -logFile  -batchmode
