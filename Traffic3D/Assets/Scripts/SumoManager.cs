a5edfc07c351a903937d26b99a29d4dc276fa4d4
Callum Bugajski <callumbugajski@hotmail.com>
2020-02-14 17:35:14 +0000

    Imported all Sumo items and started basic link
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/84>

---
+++ Traffic3D/Assets/Scripts/SumoManager.cs
@@ -0,0 +1,77 @@
+﻿using CodingConnected.TraCI.NET;
+using CodingConnected.TraCI.NET.Types;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SumoManager : MonoBehaviour
+{
+    private TraCIClient client;
+    private VehicleFactory vehicleFactory;
+    private Dictionary<string, Rigidbody> renderedVehicles = new Dictionary<string, Rigidbody>();
+
+    void Start()
+    {
+        vehicleFactory = FindObjectOfType<VehicleFactory>();
+        string filePath = System.IO.Path.Combine(Application.dataPath, "Sumo");
+        ImportAndGenerate.parseXMLfiles(filePath);
+        ImportAndGenerate.CreateStreetNetwork();
+        client = new TraCIClient();
+        if (client.Connect("127.0.0.1", 4001))
+        {
+            Debug.Log("Connected to Sumo");
+        }
+        else
+        {
+            Debug.Log("Unable to connect to Sumo");
+            this.enabled = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        client.Control.SimStep(0.0);
+        TraCIResponse<List<string>> vehicleIDs = client.Vehicle.GetIdList();
+        foreach (string vehicleId in vehicleIDs.Content)
+        {
+            if (!renderedVehicles.ContainsKey(vehicleId))
+            {
+                CreateRenderedVehicle(vehicleId);
+            }
+            Position3D position = client.Vehicle.GetPosition3D(vehicleId).Content;
+            double yawAngle = client.Vehicle.GetAngle(vehicleId).Content;
+            UpdateRenderedVehicle(vehicleId, position, (float)yawAngle);
+        }
+        List<string> vehiclesToDestroy = new List<string>();
+        foreach (string id in renderedVehicles.Keys)
+        {
+            if (!vehicleIDs.Content.Contains(id))
+            {
+                vehiclesToDestroy.Add(id);
+            }
+        }
+        foreach (string id in vehiclesToDestroy)
+        {
+            DestroyRenderedVehicle(id);
+        }
+
+    }
+
+    public void DestroyRenderedVehicle(string id)
+    {
+        Destroy(renderedVehicles[id].gameObject);
+        renderedVehicles.Remove(id);
+    }
+
+    public void CreateRenderedVehicle(string id)
+    {
+        Rigidbody vehicle = Instantiate(vehicleFactory.GetRandomVehicle());
+        renderedVehicles.Add(id, vehicle);
+    }
+
+    public void UpdateRenderedVehicle(string id, Position3D position3D, float angle)
+    {
+        renderedVehicles[id].transform.position = new Vector3((float)position3D.X, (float)position3D.Z, (float)position3D.Y);
+        renderedVehicles[id].transform.rotation = Quaternion.Euler(0, angle, 0);
+    }
+
+}
