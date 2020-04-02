bf9299b1c8b90545afd854fc67c77f4d786fcae6
Callum Bugajski <callumbugajski@hotmail.com>
2020-04-02 17:30:34 +0100

    Added Rain to Environment Settings on Sumo
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/90>

---
+++ Traffic3D/Assets/Scripts/EnvironmentSettings.cs
@@ -0,0 +1,23 @@
+﻿using DigitalRuby.RainMaker;
+using UnityEngine;
+
+public class EnvironmentSettings : MonoBehaviour
+{
+    public bool rain;
+    public bool snow;
+    public double lightLevel;
+
+    void Start()
+    {
+        if (rain)
+        {
+            GameObject rainPrefab = Resources.Load<GameObject>("Models/Rain");
+            foreach (Camera cam in FindObjectsOfType<Camera>())
+            {
+                GameObject rainPrefabInstance = GameObject.Instantiate(rainPrefab, cam.transform.position, Quaternion.Euler(new Vector3(0, 0, 0)));
+                rainPrefabInstance.GetComponent<RainScript>().Camera = cam;
+            }
+        }
+    }
+
+}
