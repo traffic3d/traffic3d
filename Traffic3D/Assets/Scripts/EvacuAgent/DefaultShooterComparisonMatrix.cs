d023fa4f0b937046b758198c4dea327d13c76a50
leeder.josh <leederjl@aston.ac.uk>
2021-02-19 11:46:13 +0000

    Adds seeking and searching behaviour to shooter
    Seeking behaviour:
    * If there are agents in visible pedestrian list then follow the closest.
    * Has short delay to shooter does not jump quickly between targets.
    
    Searching behaviour:
    * Uses weighted sum ranking to rank pedestrian points in given radius.
    * Footfall is beneficial.
    * Distance to pedestrian point from current location is non-beneficial.
    * The system maximises beneficial and minimises non-beneficial values.
    * Produces an array of points of given size to travel to based on rank.
    
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/136>

---
+++ Traffic3D/Assets/Scripts/EvacuAgent/DefaultShooterComparisonMatrix.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefaultShooterComparisonMatrix
+{
+    public float[,] GetDeaultComparisonMatrix()
+    {
+        return new float[,]
+        {
+            {1,  5},
+            {0.2f, 1 }
+        };
+    }
+}
