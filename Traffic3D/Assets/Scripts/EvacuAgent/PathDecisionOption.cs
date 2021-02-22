0345081ebeb1d048c71ff34c3b536ffe4611aac3
leeder.josh <leederjl@aston.ac.uk>
2021-02-22 09:55:40 +0000

    Refactors PedestrianPathCreator
    Moves the following into their own files:
    * CriteriaValues.
    * PathDecisionOption.
    * PathDecisionNode.
    
    Refactors:
    * PedestrianPathCreator by making footfall and distance private.
    * Pedestrian with clearer separation between Traffic3D and evacuation logic.
    * PedestrianPathCreatorTests with more meaningful variable names.
    
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/136>

---
+++ Traffic3D/Assets/Scripts/EvacuAgent/PathDecisionOption.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+public class PathDecisionOption
+{
+    public PedestrianPoint PedestrianPoint { get; set; }
+    public int PathDecisionRank { get; set; }
+    public float WeightedSumOfPathNodes { get; set; }
+    public List<PathDecisionNode> PathDecisionNodes { get; set; }
+
+    public PathDecisionOption()
+    {
+        PathDecisionNodes = new List<PathDecisionNode>();
+    }
+}
