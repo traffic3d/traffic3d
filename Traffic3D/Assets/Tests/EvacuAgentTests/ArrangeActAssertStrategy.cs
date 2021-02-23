0066b87c4fe67433b7ac162ba1db59c2a9884189
leeder.josh <leederjl@aston.ac.uk>
2021-02-23 17:55:43 +0000

    Adds ArrangeActAssertStrategy for tests
    Refactors CriteriaValueTests to use ArrangeActAssertStrategy.
    
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/169>

---
+++ Traffic3D/Assets/Tests/EvacuAgentTests/ArrangeActAssertStrategy.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using Tests;
+using UnityEngine.TestTools;
+
+public abstract class ArrangeActAssertStrategy : EvacuAgentCommonSceneTest
+{
+    public abstract IEnumerator PerformTest();
+    public abstract void Arrange();
+    public abstract void Act();
+    public abstract IEnumerator Assertion();
+}
