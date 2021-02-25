d07ae031b01a88e06cf73f3dd7e2be34bad8a9e5
leeder.josh <leederjl@aston.ac.uk>
2021-02-25 14:33:37 +0000

    Adds animated menu with no functionality
    * Simulation time duration is displayed in game view.
    * Menu panel added with two menu tabs: Simulation and Information.
    * Button added to minimise and maximise the panel and contents.
    * Animations for minimising and maximising the menu working.
    
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/137>

---
+++ Traffic3D/Assets/Scripts/EvacuAgent/UIController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private Text timerTextComponent;
+
+    [SerializeField]
+    private GameObject menuSimulationPanel;
+
+    [SerializeField]
+    private GameObject menuInformationPanel;
+
+    [SerializeField]
+    private GameObject menuPanelParent;
+
+    [SerializeField]
+    private Animator menuPanelParentAnimator;
+
+    private GameObject currentMenuContentPanel;
+    private const string isMenuMinimisedAnimatorString = "isMenuMinimised";
+
+    private void Start()
+    {
+        currentMenuContentPanel = menuSimulationPanel;
+        currentMenuContentPanel.SetActive(true);
+    }
+
+    void Update()
+    {
+        timerTextComponent.text = Time.timeSinceLevelLoad.ToString();
+    }
+
+    public void OnButtonClickChangeMenuContentState(int menuContentState)
+    {
+        currentMenuContentPanel.SetActive(false);
+
+        switch ((MenuContentState)menuContentState)
+        {
+            case MenuContentState.SimulationMenuContent: currentMenuContentPanel = menuSimulationPanel;
+                break;
+            case MenuContentState.InformationMenuContent:
+                currentMenuContentPanel = menuInformationPanel;
+                break;
+        }
+
+        currentMenuContentPanel.SetActive(true);
+        OpenMinimisedMenuOnMenuTabClick();
+    }
+
+    public void MinimiseMaximiseMenu()
+    {
+        bool isMenuMinimised = menuPanelParentAnimator.GetBool(isMenuMinimisedAnimatorString);
+        menuPanelParentAnimator.SetBool(isMenuMinimisedAnimatorString, !isMenuMinimised);
+    }
+
+    private void OpenMinimisedMenuOnMenuTabClick()
+    {
+        bool isMenuMinimised = menuPanelParentAnimator.GetBool(isMenuMinimisedAnimatorString);
+
+        if (isMenuMinimised)
+        {
+            menuPanelParentAnimator.SetBool(isMenuMinimisedAnimatorString, !isMenuMinimised);
+        }
+    }
+}
+
+public enum MenuContentState
+{
+    SimulationMenuContent = 0,
+    InformationMenuContent = 1
+}
