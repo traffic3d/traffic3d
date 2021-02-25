ddec5aaf0f6685f6063ea6a36074d7fd59554420
leeder.josh <leederjl@aston.ac.uk>
2021-02-25 15:24:43 +0000

    Adds SimulationDataTracker to update InfoPanel
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/169>

---
+++ Traffic3D/Assets/Scripts/EvacuAgent/SimulationDataTracker.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimulationDataTracker : MonoBehaviour
+{
+    public int GetNumberOfType(System.Type type)
+    {
+        return GameObject.FindObjectsOfType(type).Length;
+    }
+
+    public int GetNumberOfObjectsWithTag(string tag)
+    {
+        return GameObject.FindGameObjectsWithTag(tag).Length;
+    }
+}
