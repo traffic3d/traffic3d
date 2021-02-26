28b2a3ed927a3329e8d470c064116a204618efb7
leeder.josh <leederjl@aston.ac.uk>
2021-02-26 10:08:46 +0000

    Adds logic for disabling FOV visuals from UI
    Refactors:
    * SimulationPanelController method calls out of UIController.
    * InformationPanelController method calls out of UIController.
    * EvacuAgentSceneParamaters from storing constants to scene paramaters.
    * SceneParamaters used to spawn agents with current FOV isEnabled toggle.
    
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/137>

---
+++ Traffic3D/Assets/Scripts/EvacuAgent/InformationPanelController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InformationPanelController : MonoBehaviour
+{
+    [SerializeField]
+    private Text NumberOfPedestriansText;
+
+    public void Update()
+    {
+        NumberOfPedestriansText.text = GetNumberOfType(typeof(Pedestrian)).ToString();
+        //NumberOfShootersText.text = simulationDataTracker.GetNumberOfObjectsWithTag(shooterTag).ToString(); commented out as the shooter tag does not exist yet
+    }
+
+    public int GetNumberOfType(System.Type type)
+    {
+        return GameObject.FindObjectsOfType(type).Length;
+    }
+
+    public int GetNumberOfObjectsWithTag(string tag)
+    {
+        return GameObject.FindGameObjectsWithTag(tag).Length;
+    }
+}
---
+++ Traffic3D/Assets/Scripts/EvacuAgent/SimulationPanelController.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class SimulationPanelController : MonoBehaviour
+{
+    public void TogglePedestrianFieldOfViewVisuals(bool isTurnedOn)
+    {
+        EvacuAgentSceneParamaters.IS_FOV_VISUAL_ENABLED = isTurnedOn;
+
+        foreach(FieldOfView fieldOfView in GameObject.FindObjectsOfType<FieldOfView>())
+        {
+            fieldOfView.GetComponent<MeshRenderer>().enabled = isTurnedOn;
+        }
+    }
+}
