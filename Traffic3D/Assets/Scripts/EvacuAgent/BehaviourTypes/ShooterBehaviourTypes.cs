66f638da375aca871e7aebd8fa59eee8e2713a98
leeder.josh <leederjl@aston.ac.uk>
2021-03-09 09:10:19 +0000

    Fixes spelling mistakes as per comments
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/142>

--- Traffic3D/Assets/Scripts/EvacuAgent/BehaviourTypes/ShooterBehaviourTypes.cs
+++ Traffic3D/Assets/Scripts/EvacuAgent/BehaviourTypes/ShooterBehaviourTypes.cs
@@ -1,5 +1,7 @@
-﻿// Note that order matters. The closer to 0 in the enum the higher preference it has in the subsumption hierarchy
-// Meaning it will be iterated over first and potentially chosen over other behaviours
+﻿/*Note that order matters. The closer to 0 in the enum the higher preference it has in the subsumption hierarchy
+* Meaning it will be iterated over first and potentially chosen over other behaviours
+* 1f here is the chance the behaviour has of being added to a BehaviourCollection (this is not used yet, 09/03/21)
+*/
 public enum ShooterBehaviourTypes
 {
     [BehaviourType("FollowClosestTargetBehaviour", 1f)]
