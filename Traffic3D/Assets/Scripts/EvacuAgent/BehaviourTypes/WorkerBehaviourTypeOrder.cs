58512bb2b31d01569bd36fd9e4ee8f7a3e697e4f
leeder.josh <leederjl@aston.ac.uk>
2021-03-16 10:12:34 +0000

    Adds Worker and Generic Behaviours
    Adds:
    
    * GenericEnterLeaveBuildingBehaviour - Interacts with
    PedestrianPointBuildingEnterence to allow Pedestrians to enter
    buildings for a short time before moving to their next destination.
    
    * GenericMoveToNextDestinationBehaviour - Updates a Pedestrians
    destination if they are within proximity to their current destination.
    
    * GenericNoNewBehaviour - Stops exceptions being thrown when no
    BehaviourStrategy are triggered in a fixedUpdate.
    
    * GenericPathCreationBehaviour - Creates and holds a path of
    PedestrianPoint used by Pedestrians.
    
    * PedestrianPointPathCreator - Used by GenericPathCreationBehaviour
    to create a path based on PedestrianType.
    
    * PedestrianPointType - Used by PedestrianPoint to allocate different
    wait times in buildings and for path finding based on PedestrianType.
    
    * PedestrianType - Used to calculate paths incorporating different types
    of nodes.
    
    Refactors:
    
    * FieldOfView - Into the perception folder.
    
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/147>

---
+++ Traffic3D/Assets/Scripts/EvacuAgent/BehaviourTypes/WorkerBehaviourTypeOrder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+public class WorkerBehaviourTypeOrder : BehaviourTypeOrder
+{
+    private void Start()
+    {
+        behaviourTypes = new List<BehaviourType>()
+        {
+            new GenericPathCreationBehaviourType(),
+            new GenericEnterLeaveBuildingBehaviourType(),
+            new GenericMoveToNextDestinationBehaviourType(),
+            new GenericNoNewBehaviourType()
+        };
+    }
+
+    public override List<BehaviourType> GetBehaviourTypes() => behaviourTypes;
+
+    private class GenericEnterLeaveBuildingBehaviourType : BehaviourType
+    {
+        private readonly string behaviourStrategyName = "GenericEnterLeaveBuildingBehaviour";
+        private readonly float behaviourStrategyChanceToUse = 1f;
+
+        public override string GetBehaviourStrategyName() => behaviourStrategyName;
+        public override float GetBehaviourStrategyChance() => behaviourStrategyChanceToUse;
+    }
+
+    private class GenericPathCreationBehaviourType : BehaviourType
+    {
+        private readonly string behaviourStrategyName = "GenericPathCreationBehaviour";
+        private readonly float behaviourStrategyChanceToUse = 1f;
+
+        public override string GetBehaviourStrategyName() => behaviourStrategyName;
+        public override float GetBehaviourStrategyChance() => behaviourStrategyChanceToUse;
+    }
+
+    private class GenericMoveToNextDestinationBehaviourType : BehaviourType
+    {
+        private readonly string behaviourStrategyName = "GenericMoveToNextDestinationBehaviour";
+        private readonly float behaviourStrategyChanceToUse = 1f;
+
+        public override string GetBehaviourStrategyName() => behaviourStrategyName;
+        public override float GetBehaviourStrategyChance() => behaviourStrategyChanceToUse;
+    }
+
+    private class GenericNoNewBehaviourType : BehaviourType
+    {
+        private readonly string behaviourStrategyName = "GenericNoNewBehaviour";
+        private readonly float behaviourStrategyChanceToUse = 1f;
+
+        public override string GetBehaviourStrategyName() => behaviourStrategyName;
+        public override float GetBehaviourStrategyChance() => behaviourStrategyChanceToUse;
+    }
+}
---
+++ Traffic3D/Assets/Scripts/EvacuAgent/GenericBehaviours/GenericMoveToNextDestinationBehaviour.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.AI;
+
+public class GenericMoveToNextDestinationBehaviour : BehaviourStrategy
+{
+    public PedestrianPoint currentPedestrianPointDestination { get; private set; }
+    private GenericPathCreationBehaviour genericPathCreationBehaviour;
+    private NavMeshAgent navMeshAgent;
+    private int currentPathIndex;
+    private readonly float proximityToDestination = 1f;
+
+    private void Start()
+    {
+        genericPathCreationBehaviour = GetComponent<GenericPathCreationBehaviour>();
+        navMeshAgent = GetComponentInParent<NavMeshAgent>();
+        currentPathIndex = 0;
+    }
+
+    public override bool ShouldTriggerBehaviour()
+    {
+        if (navMeshAgent.remainingDistance < proximityToDestination)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public override void PerformBehaviour()
+    {
+        if (currentPathIndex + 1 <= genericPathCreationBehaviour.pathOfPedestrianPoints.Count)
+        {
+            currentPedestrianPointDestination = genericPathCreationBehaviour.pathOfPedestrianPoints[currentPathIndex];
+            navMeshAgent.SetDestination(currentPedestrianPointDestination.GetPointLocation());
+            currentPathIndex++;
+        }
+    }
+}
