5ef9c8731fd7e00c4ec273a9d00a0e292c783146
leeder.josh <leederjl@aston.ac.uk>
2021-03-22 14:22:03 +0000

    Fixes Path creation tests
    Fixes broken references in PedestrianPointPathCreatorTests and changes
    then to specific WorkerPedestrianPointPathCreatorTests, also removes
    redundant shooter path test.
    
    All shooter path creation tests are now in ShooterPathCreatorTests,
    fixes break due to differnt path sizes in script and test and alters
    expected nodes in tests.
    
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/148>

--- Traffic3D/Assets/Tests/EvacuAgentTests/PedestrianTypeFactoryTests.cs
+++ Traffic3D/Assets/Tests/EvacuAgentTests/PedestrianTypeFactoryTests.cs
@@ -1,7 +1,54 @@
 ﻿using NUnit.Framework;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.TestTools;
+
+public class AbstractEvacuAgentPedestrianFactory_CorrectlyCreatesBehaviourCollection : ArrangeActAssertStrategy
+{
+    private BehaviourTypeOrder mockBehaviourTypeOrder;
+    private WorkerPedestrianFactory workerPedestrianFactory;
+    private BehaviourCollection behaviourCollection;
+    private BehaviourController behaviourController;
+    private List<BehaviourStrategy> actualBehaviourStrategies;
+
+    [UnityTest]
+    public override IEnumerator PerformTest()
+    {
+        yield return null;
+        Arrange();
+        Act();
+        Assertion();
+    }
+
+    public override void Arrange()
+    {
+        mockBehaviourTypeOrder = BehaviourCollectionFactoryTestsHelper.GetMockBehaviourTypeOrder();
+        behaviourController = BehaviourCollectionFactoryTestsHelper.GetBehaviourController();
+        behaviourController.isUpdateOn = false;
+        workerPedestrianFactory = BehaviourCollectionFactoryTestsHelper.GetBehaviourCollectionFactory();
+        actualBehaviourStrategies = new List<BehaviourStrategy>();
+    }
+
+    public override void Act()
+    {
+        behaviourCollection = workerPedestrianFactory.GenerateBehaviourCollection(behaviourController, mockBehaviourTypeOrder);
+
+        foreach (BehaviourStrategy behaviourStrategy in behaviourCollection.behaviours)
+        {
+            behaviourStrategy.enabled = false;
+            actualBehaviourStrategies.Add(behaviourStrategy);
+        }
+    }
+
+    public override void Assertion()
+    {
+        Assert.IsNotNull(behaviourCollection);
+        Assert.AreEqual(2, behaviourCollection.behaviours.Count);
+        Assert.IsInstanceOf(typeof(FollowClosestTargetBehaviour), actualBehaviourStrategies[0]);
+        Assert.IsInstanceOf(typeof(MoveToNextDestinationBehaviour), actualBehaviourStrategies[1]);
+    }
+}
 
 public class WorkerPedestrianFactory_AddEvacuAgentBehaviour_CorrectlyAddsWorkerBehaviours : ArrangeActAssertStrategy
 {
@@ -92,5 +139,65 @@ public class ShooterPedestrianFactory_AddEvacuAgentBehaviour_CorrectlyAddsShoote
         Assert.NotNull(shooterPedestrian.behaviourController.behaviourCollections);
         Assert.IsInstanceOf(typeof(ShooterBehaviourTypeOrder), shooterPedestrian.behaviourTypeOrder);
         Assert.IsInstanceOf(typeof(ShooterPedestrianPointPathCreator), shooterPedestrian.PedestrianPointPathCreator);
+    }
+}
+
+public static class BehaviourCollectionFactoryTestsHelper
+{
+    public static BehaviourTypeOrder GetMockBehaviourTypeOrder()
+    {
+        GameObject shooterBehaviourTypeOrderGameObject = GameObject.Instantiate(new GameObject());
+        return shooterBehaviourTypeOrderGameObject.AddComponent<MockBehaviourTypeOrder>();
+    }
+
+    public static WorkerPedestrianFactory GetBehaviourCollectionFactory()
+    {
+        return GameObject.FindObjectOfType<WorkerPedestrianFactory>();
+    }
+
+    public static BehaviourController GetBehaviourController()
+    {
+        GameObject behaviourControllerGameObject = GameObject.Instantiate(new GameObject());
+        return behaviourControllerGameObject.AddComponent<BehaviourController>();
+    }
+
+    private class MockBehaviourTypeOrder : BehaviourTypeOrder
+    {
+        public override List<BehaviourType> GetBehaviourTypes()
+        {
+            return new List<BehaviourType>
+            {
+                new MockFollowClosestTargetBehaviourType(),
+                new MockCreateWeightedPathOfPedestrianPointsBehaviourType(),
+                new MockMoveToNextDestinationBehaviourtBehaviourType()
+            };
+        }
+
+        private class MockFollowClosestTargetBehaviourType : BehaviourType
+        {
+            private readonly string behaviourStrategyName = "FollowClosestTargetBehaviour";
+            private readonly float behaviourStrategyChanceToUse = 1f;
+
+            public override string GetBehaviourStrategyName() => behaviourStrategyName;
+            public override float GetBehaviourStrategyChance() => behaviourStrategyChanceToUse;
+        }
+
+        private class MockCreateWeightedPathOfPedestrianPointsBehaviourType : BehaviourType
+        {
+            private readonly string behaviourStrategyName = "CreateWeightedPathOfPedestrianPointsBehaviour";
+            private readonly float behaviourStrategyChanceToUse = 0f;
+
+            public override string GetBehaviourStrategyName() => behaviourStrategyName;
+            public override float GetBehaviourStrategyChance() => behaviourStrategyChanceToUse;
+        }
+
+        private class MockMoveToNextDestinationBehaviourtBehaviourType : BehaviourType
+        {
+            private readonly string behaviourStrategyName = "MoveToNextDestinationBehaviour";
+            private readonly float behaviourStrategyChanceToUse = 1f;
+
+            public override string GetBehaviourStrategyName() => behaviourStrategyName;
+            public override float GetBehaviourStrategyChance() => behaviourStrategyChanceToUse;
+        }
     }
 }
