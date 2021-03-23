e6a33f0f1fa6ad6fdfda86493ede80a1315f16d8
leeder.josh <leederjl@aston.ac.uk>
2021-03-23 12:17:26 +0000

    Fixes CreateEvacuAgentPedestrian spelling
    Tested-by: Bob The Beautiful Canoe Robot <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/148>
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/148>

--- Traffic3D/Assets/Scripts/EvacuAgent/Factories/WorkerPedestrianFactory.cs
+++ Traffic3D/Assets/Scripts/EvacuAgent/Factories/WorkerPedestrianFactory.cs
@@ -2,7 +2,7 @@
 
 public class WorkerPedestrianFactory : AbstractEvacuAgentPedestrianFactory
 {
-    public override EvacuAgentPedestrianBase CreateEvacuaAgentPedestrian(Pedestrian pedestrian)
+    public override EvacuAgentPedestrianBase CreateEvacuAgentPedestrian(Pedestrian pedestrian)
     {
         return CreatePedestrianType(pedestrian, EvacuAgentSceneParamaters.IS_WORKER_HIGHTLIGHT_VISUAL_ENABLED, pedestrianTypePrefab);
     }
