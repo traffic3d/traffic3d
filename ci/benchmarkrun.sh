c25e23043563ee72b32c257aa36cb4a65ac11c94
Sarah Mount <s.mount@aston.ac.uk>
2021-04-26 15:16:10 +0100

    All CI scripts use 4-space indents
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/150>

--- ci/benchmarkrun.sh
+++ ci/benchmarkrun.sh
@@ -2,4 +2,11 @@
 set -e
 set -x
 echo "Running Unity"
-${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity}  -projectPath $(pwd)/Traffic3D  -executeMethod CustomCommandLineArguments.Run  -OpenScene "Scenes/DayDemo.unity"  -RunBenchmark true  -logFile  -batchmode  -quit
+${UNITY_EXECUTABLE:-xvfb-run --auto-servernum --server-args='-screen 0 640x480x24' /opt/Unity/Editor/Unity} \
+    -projectPath $(pwd)/Traffic3D \
+    -executeMethod CustomCommandLineArguments.Run \
+    -OpenScene "Scenes/DayDemo.unity" \
+    -RunBenchmark true \
+    -logFile \
+    -batchmode \
+    -quit
