767aa26d4bba582a46f55f486301d30332ec5414
Callum Bugajski <callumbugajski@hotmail.com>
2021-06-04 16:08:47 +0100

    Add random number generator
    Add dedicated random number generator to the system
    Change NextInt to use Range method
    
    Change NextInt to use local range method
    Change to System.Random
    
    Change unity random to system random
    Move field and add class documentation
    
    Move System.Random field with other fields
    Add class documentation to RandomNumberGenerator
    
    Tested-by: Bob The Beautiful Canoe Robot <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/157>
    Part-of: <https://gitlab.com/traffic3d/traffic3d/-/merge_requests/157>

---
+++ Traffic3D/Assets/Scripts/RandomNumberGenerator.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+/// <summary>
+/// The random number generator is used to centralise a System.Random object and ensure the simulation is repeatable.
+/// The RandomNumberGenerator class implements the singleton pattern.
+///
+/// Note that the System.Random object is not thread safe so use a synchronization object
+/// to ensure that only one thread can access the random number generator at a time.
+///
+/// Find more information about the System.Random class here:
+/// https://docs.microsoft.com/en-us/dotnet/api/system.random?view=net-5.0
+/// </summary>
+public class RandomNumberGenerator
+{
+    public static int seed = 123;
+    private static RandomNumberGenerator instance;
+    private System.Random random;
+
+    public static RandomNumberGenerator GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new RandomNumberGenerator();
+        }
+        return instance;
+    }
+
+    private RandomNumberGenerator()
+    {
+        Random.InitState(seed);
+        random = new System.Random(seed);
+    }
+
+    /// <summary>
+    /// Returns the next random integer within the bounds of 0 (inclusive) and bound (exclusive).
+    /// </summary>
+    /// <param name="bound">The highest bound for the range to use</param>
+    /// <returns>Returns the next random integer within the bounds of 0 (inclusive) and bound (exclusive).</returns>
+    public int NextInt(int bound)
+    {
+        return random.Next(bound);
+    }
+
+    /// <summary>
+    /// Returns the next random float between 0 and 1 inclusive.
+    /// </summary>
+    /// <returns>Returns the next random float between 0 and 1 inclusive.</returns>
+    public float NextFloat()
+    {
+        return (float) random.NextDouble();
+    }
+
+    /// <summary>
+    /// Return a random integer between min (inclusive) and max (exclusive)
+    /// </summary>
+    /// <param name="min">min number in range</param>
+    /// <param name="max">max number in range</param>
+    /// <returns>Return a random integer between min (inclusive) and max (exclusive)</returns>
+    public int Range(int min, int max)
+    {
+        return random.Next(min, max);
+    }
+
+    /// <summary>
+    /// Return a random float between min (inclusive) and max (inclusive)
+    /// </summary>
+    /// <param name="min">min number in range</param>
+    /// <param name="max">max number in range</param>
+    /// <returns>Return a random float between min (inclusive) and max (inclusive)</returns>
+    public float Range(float min, float max)
+    {
+        return (float) (min + (random.NextDouble() * (max - min)));
+    }
+
+}
